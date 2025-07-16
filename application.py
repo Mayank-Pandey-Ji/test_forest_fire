aee5fb2bb50221f2c1a11b5ac85b6f112abf558b
Mayank-Pandey-Ji <pandeymayank1099@gmail.com>
2025-07-16 22:48:46 +0530

    added CSS

--- application.py
+++ application.py
@@ -13,11 +13,7 @@ ridge_model = pickle.load(open('models/ridge.pkl','rb'))
 standard_scaler = pickle.load(open('models/scaler.pkl','rb'))
 
 
-@app.route("/")
-def index():
-    return render_template('index.html')
-
-@app.route('/predictdata',methods=['GET','POST'])
+@app.route('/',methods=['GET','POST'])
 def predict_datapoint():
     if request.method=='POST':
         Temperature=float(request.form.get('Temperature'))
@@ -32,10 +28,10 @@ def predict_datapoint():
 
         new_scaled_data = standard_scaler.transform([[Temperature, RH, Ws, Rain, FFMC, DMC, ISI, Classes, Region]])
         result = ridge_model.predict(new_scaled_data)
-        return render_template('home.html',results = result[0])
+        return render_template('index.html',results = result[0])
     else :
         return render_template('home.html')
 
 if __name__=="__main__":
     port = int(os.environ.get("PORT", 5000))  # default to 5000 for local
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000 , debug=True)
